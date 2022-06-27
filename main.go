9234812439278bc869898f265cbed1f845b17dce
yz666-12 <83385802+yz666-12@users.noreply.github.com>
2022-06-27 23:15:50 +0800

    Update main.go

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ import (
 	//                               vvvv                               //
 
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ai_false"       // 服务器监控
-	_ "https://steamstats.cn/xi"                                  // 今日喜加一
+	_ "steamstats.cn/xi"                                          // 今日喜加一
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/aiwife"         // 随机老婆
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/b14"            // base16384加解密
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/baidu"          // 百度一下
